7662ed829e3d47bda5a33b000b5d0d6ac9f49ca4
paul cannon <paul@riptano.com>
2010-11-18 15:57:50 -0700

    first pass at packaging

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(stomper):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="stomper" command="/usr/bin/stomper"
--- debian/stomper.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the stomper package
-#
-0 4	* * *	root	[ -x /usr/bin/stomper_maintenance ] && /usr/bin/stomper_maintenance
